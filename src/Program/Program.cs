9b0b8dedc742447235b7bd3ea6a679608d1177ff
cardo88 <cardo88@gmail.com>
2022-06-11 14:35:59 -0300

    battala crea usuarios, board, y posiciona ships

--- src/Program/Program.cs
+++ src/Program/Program.cs
@@ -7,21 +7,22 @@ namespace Program
     {
         static void Main(string[] args)
         {
-            Board board = new Board();
-            Batlle batlle = new Batlle();
-            IShip battleShip = new BattleShip();
+            //Board board = new Board();
+            //Batlle batlle = new Batlle();
+            //IShip battleShip = new BattleShip();
             //IShip airCraftCarrier = new AirCraftCarrier();
             
             // board.ShowBoard();
             // Console.WriteLine("____________________");
            // batlle.SetPosition(board, battleShip, 1, 4,"horizontal");
             //batlle.SetPosition(board, airCraftCarrier, 7, 2, "horizontal");
-            board.ShowBoard();
+            //board.ShowBoard();
             // batlle.EspecialBombAttack(2, 4, board);
 
-            CombineImage combineImage = new CombineImage();
-            combineImage.MergeMultipleImages(@"Background.jpg", @"AircraftCarrierH5.png", 163, 209);
+            //CombineImage combineImage = new CombineImage();
+            //combineImage.MergeMultipleImages(@"Background.jpg", @"AircraftCarrierH5.png", 163, 209);
 
+            Match partida = new Match();
 
         } 
     }
