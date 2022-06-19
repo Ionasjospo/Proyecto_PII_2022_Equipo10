aff86ad4b49d0015ccda83329a44b06a7f241982
IonasJospo <ionas.josponis@correo.ucu.edu.uy>
2022-06-19 16:33:13 -0300

    test

---
+++ src/Test/Library.Test/ShipsSetPositionTest.cs
@@ -0,0 +1,58 @@
+using System;
+using Library;
+using NUnit.Framework;
+using System.Collections.Generic;
+
+namespace Library.Test
+{
+    public class ShipsSetPositionTest
+    {
+        [SetUp]
+        public void Setup()
+        {
+            
+            
+        }
+
+        //[Test]
+        //public void SubmarineSetPosition()
+        //{
+//             Board board = new Board();
+            
+//             board.SetPosition(board.Ship[0], 0, 1, "horizontal");
+
+//             // string [,] finalBoard = new string [10, 10];
+//             // Submarine submarine = new Submarine();
+//             // for (int filas = 0; filas < 10; filas++)
+//             // {
+//             //     for (int col = 0; col < 10; col++)
+//             //     {
+//             //         finalBoard[filas, col] = "O";
+//             //     }
+//             // }
+
+//             // finalBoard[0, 1] = submarine.LetterId;
+//             // finalBoard[0, 2] = submarine.LetterId;
+//             // string expected = board.Ocean.ToString();
+//             //Console.WriteLine(finalBoard.);
+
+
+//             //Assert.IsTrue( (board.Ocean[0, 1] == "S" ) );
+//             List <string> list = new List<string>(); 
+//             for (int x = 0; x < 10 ; x++)
+//             {
+//                 for (int y = 0; y < 10; y++)
+//                 {
+//                     list.Add(board.Ocean[x, y]);
+//                 }
+//             }
+//             Assert.Contains("S", list);
+//         }
+    
+   }
+}
+
+    
+
+
+
