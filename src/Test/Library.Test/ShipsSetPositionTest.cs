8c2807de4d9ae0f65903f9423f3877a2475f3331
IonasJospo <ionas.josponis@correo.ucu.edu.uy>
2022-06-20 18:39:16 -0300

    shipsSetPosition

--- src/Test/Library.Test/ShipsSetPositionTest.cs
+++ src/Test/Library.Test/ShipsSetPositionTest.cs
@@ -14,41 +14,29 @@ namespace Library.Test
             
         }
 
-        //[Test]
-        //public void SubmarineSetPosition()
-        //{
-//             Board board = new Board();
+        [Test]
+        public void ShipsSetPosition()
+        {
+            User juan = new User("Juan");
+            juan.NewMatch(false);
+            Match match =  MatchList.Instance.HistoricMatches[0];
+            BoardWithShips boardShip = match.PlayerA1.BoardWithShips as BoardWithShips;
+            boardShip.SetPosition(boardShip.Ship[0], 0, 0, "horizontal");
+            // boardShip.SetPosition(boardShip.Ship[1], 0, 2, "horizontal");
+            // boardShip.SetPosition(boardShip.Ship[2], 0, 4, "horizontal");
+            // boardShip.SetPosition(boardShip.Ship[3], 0, 6, "horizontal");
+
+            // string [,] finalBoard = new string [10, 10];
             
-//             board.SetPosition(board.Ship[0], 0, 1, "horizontal");
-
-//             // string [,] finalBoard = new string [10, 10];
-//             // Submarine submarine = new Submarine();
-//             // for (int filas = 0; filas < 10; filas++)
-//             // {
-//             //     for (int col = 0; col < 10; col++)
-//             //     {
-//             //         finalBoard[filas, col] = "O";
-//             //     }
-//             // }
-
-//             // finalBoard[0, 1] = submarine.LetterId;
-//             // finalBoard[0, 2] = submarine.LetterId;
-//             // string expected = board.Ocean.ToString();
-//             //Console.WriteLine(finalBoard.);
-
-
-//             //Assert.IsTrue( (board.Ocean[0, 1] == "S" ) );
-//             List <string> list = new List<string>(); 
-//             for (int x = 0; x < 10 ; x++)
-//             {
-//                 for (int y = 0; y < 10; y++)
-//                 {
-//                     list.Add(board.Ocean[x, y]);
-//                 }
-//             }
-//             Assert.Contains("S", list);
-//         }
-    
+            
+            // finalBoard[0, 1] = boardShip.Ship[0].LetterId;
+            // finalBoard[0, 2] = boardShip.Ship[0].LetterId;
+            
+            // Assert.AreEqual("S",boardShip.Ocean[0, 1].ToString());
+            // Assert.AreEqual("S", boardShip.Ocean[0, 2].ToString());
+            
+            
+        }
    }
 }
 
